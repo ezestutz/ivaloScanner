13788e03b2d1cd149134f94953dc9f7d7241de02
ezestutz <ezequielo.stutzdiaz@gmail.com>
2021-06-02 22:55:43 -0300

    login scan & functionality

--- App.tsx
+++ App.tsx
@@ -9,12 +9,18 @@ import {
   SafeAreaView,
   TouchableOpacity,
   ActivityIndicator,
-  Modal,
+  Image,
   ToastAndroid,
+  StyleSheet,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import axios from 'axios';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faBarcode, faSignInAlt} from '@fortawesome/free-solid-svg-icons';
+import Logo from './assets/ivalo.png';
+
+library.add(faSignInAlt, faBarcode);
 
 export default function App() {
   const [loadingLogin, setLoadingLogin] = useState(true);
@@ -46,46 +52,136 @@ export default function App() {
     } else {
       AsyncStorage.setItem('TOKEN', Token, () => {
         setToken(Token);
-        setLoadingLogin(false);
       });
     }
+    setLoadingLogin(false);
+  };
+
+  const onBarcodeScan = (barcode: string) => {
+    console.log(barcode);
   };
 
   return (
-    <SafeAreaView>
-      {(loadingLogin || loadingScan) && (
-        <Modal style={{flex: 1}}>
-          <ActivityIndicator color="#000000" />
-        </Modal>
-      )}
-      {!token ? (
+    <SafeAreaView style={styles.container}>
+      {token ? (
+        <View style={styles.subContainer}>
+          <Image source={Logo} style={[styles.logo, styles.logoMargins]} />
+          {loadingScan ? (
+            <ActivityIndicator size={50} color="#2baabe" />
+          ) : (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => onBarcodeScan('need to add camera here')}>
+              <Text style={styles.buttonText}>Escanear</Text>
+              <FontAwesomeIcon color="#007bff" icon={faBarcode} />
+            </TouchableOpacity>
+          )}
+        </View>
+      ) : (
         <KeyboardAvoidingView behavior="padding">
           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View>
-              <TextInput
-                placeholder="Username"
-                onChangeText={text => setUsername(text)}
-              />
-              <TextInput
-                placeholder="Password"
-                secureTextEntry={true}
-                onChangeText={text => setPassword(text)}
-              />
-              <TouchableOpacity onPress={() => onLoginPress()}>
-                <Text>Ingresar</Text>
-                <FontAwesomeIcon icon="sign-in" />
-              </TouchableOpacity>
+            <View style={styles.subContainer}>
+              <Image source={Logo} style={[styles.logo, {marginBottom: 25}]} />
+              <View style={styles.loginContainer}>
+                <Text style={styles.title}>Iniciar sesión</Text>
+                <Text style={styles.inputLabel}>Usuario</Text>
+                <TextInput
+                  placeholderTextColor="#000000"
+                  selectionColor="#000000"
+                  style={styles.input}
+                  autoCapitalize="none"
+                  onChangeText={text => {
+                    if (!loadingLogin) setUsername(text);
+                  }}
+                />
+                <Text style={styles.inputLabel}>Contraseña</Text>
+                <TextInput
+                  placeholderTextColor="#000000"
+                  selectionColor="#000000"
+                  style={styles.input}
+                  autoCapitalize="none"
+                  secureTextEntry
+                  onChangeText={text => {
+                    if (!loadingLogin) setPassword(text);
+                  }}
+                />
+                <TouchableOpacity
+                  disabled={!usuario || !contraseña || loadingLogin}
+                  onPress={() => onLoginPress()}
+                  style={styles.button}>
+                  {loadingLogin ? (
+                    <ActivityIndicator color="#007bff" />
+                  ) : (
+                    <>
+                      <Text style={styles.buttonText}>Ingresar</Text>
+                      <FontAwesomeIcon color="#007bff" icon={faSignInAlt} />
+                    </>
+                  )}
+                </TouchableOpacity>
+              </View>
             </View>
           </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
-      ) : (
-        <View style={{flex: 1, justifyContent: 'center', marginBottom: 15}}>
-          <TouchableOpacity>
-            <Text>Escanear</Text>
-            <FontAwesomeIcon icon="barcode" />
-          </TouchableOpacity>
-        </View>
       )}
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F0F8FF',
+  },
+  subContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    height: 200,
+    width: 200,
+  },
+  logoMargins: {
+    marginBottom: '15%',
+  },
+  loginContainer: {
+    padding: 35,
+    width: '90%',
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    borderColor: '#ced4da',
+    borderWidth: 1,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 15,
+    fontSize: 30,
+  },
+  inputLabel: {
+    marginBottom: 5,
+    marginTop: 15,
+    fontSize: 18,
+  },
+  input: {
+    height: 40,
+    borderRadius: 5,
+    borderColor: '#ced4da',
+    borderWidth: 1,
+    fontSize: 15,
+    color: '#333333',
+    textDecorationLine: 'none',
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    borderColor: '#007bff',
+    borderWidth: 1,
+    margin: 15,
+    height: 40,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#007bff',
+    paddingRight: 10,
+  },
+});
